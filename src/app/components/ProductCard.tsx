c77cd22b64c5fe752cb048aa2373766bc77eab85
Enci <encsi2346@gmail.com>
2024-01-07 00:44:25 +0100

    create get all api call

--- src/app/components/ProductCard.tsx
+++ src/app/components/ProductCard.tsx
@@ -14,7 +14,7 @@ const ProductCard = ({ id, title, description, price, discountPercentage, thumbn
     return (
         <div className="card card-compact w-[305px] bg-white border border-solid border-[#DBDBDB] rounded-[6.46px] m-5">
             <figure className="relative px-2 mt-2">
-                <img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" className="rounded-[6.46px] w-[282.21px] h-[149.18px]"/>
+                <img src={thumbnail} alt="Shoes" className="rounded-[6.46px] w-[282.21px] h-[149.18px]"/>
                 <div className="absolute top-2 right-5 rounded-full bg-[#6100FF] text-white font-[600] text-[14px] leading-[18.9px] py-1 px-4 z-10">{discountPercentage}%</div>
             </figure>
             <div className="card-body">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
+'use client';
 import ProductCard from "@/app/components/ProductCard";
-import Navbar from "@/app/components/Navbar";
-import {useState} from "react";
+import {useEffect, useState} from 'react';
 
 interface Product {
     id: number;
@@ -16,92 +16,47 @@ interface Product {
     images: string[];
 }
 
-const AllProductPage = async () => {
+const AllProductPage = () => {
+    const [isLoading, setIsLoading] = useState(true);
+    const [products, setProducts] = useState<Product[]>([]);
 
-
-    const res = await fetch('https://dummyjson.com/products');
-    const products: Product[] = [
-        {
-            "id": 1,
-            "title": "iPhone 1",
-            "description": "An apple mobile which is nothing like apple",
-            "price": 549,
-            "discountPercentage": 12.96,
-            "rating": 4.69,
-            "stock": 94,
-            "brand": "Apple",
-            "category": "smartphones",
-            "thumbnail": "...",
-            "images": ["...", "...", "..."]
-        },
-        {
-            "id": 2,
-            "title": "iPhone 2",
-            "description": "An apple mobile which is nothing like apple",
-            "price": 549,
-            "discountPercentage": 12.96,
-            "rating": 4.69,
-            "stock": 94,
-            "brand": "Apple",
-            "category": "smartphones",
-            "thumbnail": "...",
-            "images": ["...", "...", "..."]
-        },
-        {
-            "id": 3,
-            "title": "iPhone 3",
-            "description": "An apple mobile which is nothing like apple",
-            "price": 549,
-            "discountPercentage": 12.96,
-            "rating": 4.69,
-            "stock": 94,
-            "brand": "Apple",
-            "category": "smartphones",
-            "thumbnail": "...",
-            "images": ["...", "...", "..."]
-        },
-        {
-            "id": 4,
-            "title": "iPhone 4",
-            "description": "An apple mobile which is nothing like apple",
-            "price": 549,
-            "discountPercentage": 12.96,
-            "rating": 4.69,
-            "stock": 94,
-            "brand": "Apple",
-            "category": "smartphones",
-            "thumbnail": "...",
-            "images": ["...", "...", "..."]
-        },
-        {
-            "id": 5,
-            "title": "iPhone 5",
-            "description": "An apple mobile which is nothing like apple",
-            "price": 549,
-            "discountPercentage": 12.96,
-            "rating": 4.69,
-            "stock": 94,
-            "brand": "Apple",
-            "category": "smartphones",
-            "thumbnail": "...",
-            "images": ["...", "...", "..."]
-        },
-    ];//await res.json(); //TODO
+    useEffect(() => {
+        const getData = async () => {
+            try {
+                const response = await fetch('https://dummyjson.com/products');
+                if (!response.ok) {
+                    throw new Error('Failed to fetch data');
+                }
+                const productsData = await response.json(); //TODO
+                console.log('response', productsData);
+                setProducts(productsData.products);
+            } catch (error) {
+                console.log(error.message);
+            } finally {
+                setIsLoading(false);
+            }
+        }
+        getData();
+    }, []);
 
   return (
     <main className='grid justify-center'>
         <p className='text-[48px] font-[600] leading-[64.8px] text-[#323232] text-center m-6'>See Products</p>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-1 p-1 m-20 w-[1300px]">
-            {products.map(product => (
-                <ProductCard
-                    key={product.id}
-                    id={product.id}
-                    title={product.title}
-                    description={product.description}
-                    price={product.price}
-                    discountPercentage={product.discountPercentage}
-                    thumbnail={product.thumbnail}
-                />
+            {isLoading ? (
+                <div>Loading...</div>
+            ) : (
+                products.map(product => (
+                    <ProductCard
+                        key={product.id}
+                        id={product.id}
+                        title={product.title}
+                        description={product.description}
+                        price={product.price}
+                        discountPercentage={product.discountPercentage}
+                        thumbnail={product.thumbnail}
+                    />
+                    )
                 )
             )}
         </div>
